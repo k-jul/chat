e6791e5149de1919ec219a257cafe307cbb23a7b
julia <kuznietsova.j@gmail.com>
2018-07-13 00:54:12 +0300

    Message render implemented and styles updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,24 @@
 const btnLogIn = document.getElementById('btn-login');
 const btnRegister = document.getElementById('btn-registration');
+const btnSendMessage = document.getElementById('btn-send');
+const btnLogOut = document.getElementById('btn-logout');
 
 const inputLogIn = document.getElementById('input-login');
 const inputRegName = document.getElementById('input-reg-name');
 const inputRegEmail = document.getElementById('input-reg-email');
-const inputMessage = document.getElementById('btn-send');
+const inputMessage = document.getElementById('input-field');
 
 const aside = document.getElementById('aside');
 const loggedinUserName = document.getElementById('user-name');
 const modal = document.getElementById('cover');
+const messageContainer = document.getElementById('main');
 
 let users = [];
+let userName = '';
+let userEmail = '';
+let currentUser = {};
+let id = 0;
+let messageText = '';
 
 class User {
     constructor (id, name, email, status="offline") {
@@ -21,16 +29,28 @@ class User {
     }
 }
 
-let userName = '';
-let userEmail = '';
-let id = 0;
-
 function userCheck() {
 
     for (let i=0; i < users.length; i++) {
         if (users[i].name == userName) return users[i];
     };
         return false;
+};
+
+function renderMessage (author, content) {
+
+    let date = new Date().toDateString();
+
+    let message = document.createElement('div');
+    message.classList.add('message');
+
+    message.innerHTML = `<div class ="message-info">
+    <div class ="author">${author}</div>
+    <div class ="date">${date}</div>
+    </div>
+    <p class = "message-content">${content}</p>`
+
+    messageContainer.appendChild(message);
 }
 
 
@@ -52,13 +72,12 @@ btnLogIn.addEventListener('click', function (e) {
     e.preventDefault();
 
     if (userCheck())  {
-    let currentUser = userCheck();
+    currentUser = userCheck();
     currentUser.status = "online";
     modal.style.display='none';
 
-    loggedinUserName.innerHTML = currentUser.name;
-
-    console.log(users);
+    loggedinUserName.innerHTML = `<i class="fas fa-user-alt"></i> ${currentUser.name}`;
+    aside.innerHTML += `<p> ${currentUser.name} <sup class="user-status">${currentUser.status}</sup> </p>`;
 
     } else alert ('Please register!');
 
@@ -81,13 +100,30 @@ btnRegister.addEventListener('click', function(e) {
     id++;
 
     users.push(new User (id, userName, userEmail));
-    console.log(users);
 
     inputRegEmail.value = '';
     inputRegName.value = '';
 
 
 });
+
+inputMessage.addEventListener('change', function(e) {
+    messageText = e.target.value;
+   });
+
+btnSendMessage.addEventListener('click',  function() {
+    renderMessage(loggedinUserName.innerHTML, messageText);
+    inputMessage.value = '';
+});
+
+btnLogOut.addEventListener('click', function (e) {
+    currentUser.status = "offline";
+    modal.style.display='block';
+    console.log(users);
+});
+
+
+
 
 
 
